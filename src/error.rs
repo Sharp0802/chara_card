1f7a2abb695a7d62662e375f512a71300b0f801f
sharp0802 <sharp0802.business@gmail.com>
2026-05-05 21:40:50 +0900

    :recycle: Remove unused errors

--- src/error.rs
+++ src/error.rs
@@ -1,16 +1,7 @@
-use crate::raw::Version;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum Error {
     #[error("version string is malformed")]
     MalformedVersion,
-    #[error("unsupported version ({0})")]
-    UnsupportedVersion(Version),
-    #[error("invalid version ({1}) for specification ({0})")]
-    InvalidVersion(String, Version),
-    #[error("feature required for version ({0}) is missing")]
-    MissingFeature(Version),
-    #[error("specified asset name ({0}) is already occupied")]
-    AssetNameConflict(String),
 }
