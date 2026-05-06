c803e33d8675e76e7bd008509b9f746f9dfea946
sharp0802 <sharp0802.business@gmail.com>
2026-05-06 18:41:20 +0900

    :sparkles: Parse example responses within single serialization phase

--- src/raw/v1/data.rs
+++ src/raw/v1/data.rs
@@ -1,3 +1,4 @@
+use crate::raw::v1::examples::Examples;
 use serdev::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -8,4 +9,38 @@ pub struct CharacterCardData {
     pub scenario: String,
     pub first_mes: String,
     pub mes_example: String,
+    pub mes_example: Examples,
+    #[serde(with = "examples")]
+    pub mes_example: Vec<String>,
+}
+
+pub mod examples {
+    use super::*;
+
+    pub fn serialize<S>(value: &Vec<String>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut buffer = String::new();
+        for item in value {
+            buffer.push_str("<START>\n");
+            buffer.push_str(item);
+        }
+        serializer.serialize_str(&buffer)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Vec<String>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let raw = <&str>::deserialize(deserializer)?;
+
+        let vec: Vec<_> = raw
+            .split("<START>\n")
+            .filter(|s| !s.trim().is_empty())
+            .map(|s| s.to_owned())
+            .collect();
+
+        Ok(vec)
+    }
 }
