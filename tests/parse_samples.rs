49cead8a4c4caed8c8aec4547e57da5ba42995de
sharp0802 <sharp0802.business@gmail.com>
2026-05-06 21:19:58 +0900

    :white_check_mark: Add test cases

--- tests/parse_samples.rs
+++ tests/parse_samples.rs
@@ -2,52 +2,48 @@ use assert_json_diff::assert_json_eq;
 use chara_card::charx::CharX;
 use chara_card::raw::CharacterCard;
 use jiff::Timestamp;
+use rstest::rstest;
 use serde_json::Value;
-use std::io::Cursor;
+use std::fs::{read_to_string, File};
+use std::path::PathBuf;
 use url::Url;
 
-const CHARX: [&[u8]; 1] = [
-    include_bytes!("samples/test.charx")
-];
+#[rstest]
+fn parse_charx(#[files("tests/samples/charx/*.charx")] path: PathBuf) {
+    let file = File::open(path).unwrap();
+    let _charx = CharX::from(file).unwrap();
+}
 
-const CARDS: [&str; 2] = [
-    include_str!("samples/sample1/card.json"),
-    include_str!("samples/sample2/card.json"),
-];
+#[rstest]
+fn parse_card_json(#[files("tests/samples/json/*/card.json")] path: PathBuf) {
+    let json = read_to_string(path).unwrap();
 
-#[test]
-fn parse_samples() {
-    for charx in CHARX {
-        let reader = Cursor::new(charx);
-        let _charx = CharX::from(reader).unwrap();
+    let mut value: Value = serde_json::from_str(&json).unwrap();
+
+    // NOTE: RisuAI uses millisecond for creation timestamp
+    if let Some(creation_date) = value
+        .get_mut("data")
+        .and_then(|v| v.get_mut("creation_date"))
+    {
+        let ts = creation_date.as_i64().unwrap();
+        let ts = Timestamp::from_millisecond(ts).unwrap();
+        *creation_date = Value::Number(ts.as_second().into())
     }
 
-    for card in CARDS {
-        let mut value: Value = serde_json::from_str(card).unwrap();
+    // NOTE: It seems RisuAI doesn't encode URIs
+    if let Some(assets) = value.get_mut("data").and_then(|v| v.get_mut("assets")) {
+        for asset in assets.as_array_mut().unwrap() {
+            let Some(uri) = asset.get_mut("uri") else {
+                continue;
+            };
 
-        // NOTE: RisuAI uses millisecond for creation timestamp
-        if let Some(creation_date) = value.get_mut("data").and_then(|v| v.get_mut("creation_date")) {
-            let ts = creation_date.as_i64().unwrap();
-            let ts = Timestamp::from_millisecond(ts).unwrap();
-            *creation_date = Value::Number(ts.as_second().into())
+            let url: Url = uri.as_str().unwrap().parse().unwrap();
+            *uri = Value::String(url.to_string());
         }
-
-        // NOTE: It seems RisuAI doesn't encode URIs
-        if let Some(assets) = value.get_mut("data").and_then(|v| v.get_mut("assets")) {
-            for asset in assets.as_array_mut().unwrap() {
-                let Some(uri) = asset.get_mut("uri") else {
-                    continue;
-                };
-
-                let url: Url = uri.as_str().unwrap().parse().unwrap();
-
-                *uri = Value::String(url.to_string());
-            }
-        }
-
-        let dto: CharacterCard = serde_json::from_str(card).unwrap();
-        let ser = serde_json::to_value(dto).unwrap();
-
-        assert_json_eq!(value, ser);
     }
+
+    let dto: CharacterCard = serde_json::from_str(&json).unwrap();
+    let ser = serde_json::to_value(dto).unwrap();
+
+    assert_json_eq!(value, ser);
 }
